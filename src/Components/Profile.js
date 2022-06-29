732b447bba603b9e43453f3ee7c4160864115344
slama001 <75125561+slama1234@users.noreply.github.com>
2022-06-29 17:10:32 +0200

    Update Profile.js

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,28 +1,45 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card,Button} from 'react-bootstrap';
-import propTypes from 'prop-types';
-const Profile = ({Listprof,displayName,children}) => {
-    return (
-<div>
-  {Listprof.map((el,index)=><Card key={index} style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={children} />
-  <Card.Body>
-    <Card.Title>{el.fullName}</Card.Title>
-    <Card.Text>
-      {el.bio}
-    </Card.Text>
-    <Card.Text>
-      {el.profession}
-    </Card.Text>
-    <Button variant="primary" onClick={()=>displayName(el.fullName)} >show name</Button>
-  </Card.Body>
-</Card>)}
-</div>
-    )
-  }
+import React from "react" ;
+import PropTypes from 'prop-types';
+const ProfileData = props =>
+{
+    const styleObject = {
+        margin:"150px 350px",
+        fontSize:"20px",
+        border:"1px solid blue" ,
+        padding: "50px",
+        fontFamily:"Roboto,san-serif"
+    }
+    const styleButton = {
+        padding:"20px 40px",
+        fontSize:"20px",
+        cursor:"pointer"
+    }
+    function clickHandler()
+    {
+        alert(`your name is :${props.name}`)
+    }
+    return (<div style = {styleObject}>
+        {props.children}
+        <h2>{props.name}</h2>
+        <p><i>Bio : </i>{props.bio}</p>
+        <p><b>Profission:</b> {props.profission}</p>
+        <button onClick={clickHandler} style={styleButton}><b>Click</b></button>
+        </div>
+    );
+}
 
+ProfileData.defaultProps = {
+    name : 'Mustapha',
+    bio : `im studying web developing cuz i think it's the best industry in the world `,
+    proffision:'web development, gaming ',
+    src:'./profile.jpg'
+};
 
-export default Profile;
+ProfileData.propTypes = {
+    name:PropTypes.string,
+    bio:PropTypes.string,
+    proffision: PropTypes.string,
+    src:PropTypes.string
 
- 
+};
+export default ProfileData;
